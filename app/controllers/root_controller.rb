83f238c2c44d453b1137ae4876e4a3a09d1b1af3
snsn1024 <asqqqqqqqqqq@gmail.com>
2019-04-28 04:02:48 +0000

    fix top(new-song,new-comment show image [newest 3 choice])

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,7 +1,7 @@
 class RootController < ApplicationController
 	def top
-		@categories = Category.all 
-		@pickups = Pickup.all
+		@categories = Category.first(15)
+		@pickups = Pickup.first(5)
 		@cds = Cd.first(3)
 		@comments = Comment.first(3)
 	end
